64867ab9e8619f12486dac0635b38b85a71afcd8
Shahab <shahab.saeeda@gmail.com>
2020-07-03 01:01:31 +0430

    refactor(headercell): pass header as children instead of prop

--- src/header/HeaderCell.tsx
+++ src/header/HeaderCell.tsx
@@ -1,14 +1,14 @@
-import React, { PropsWithChildren, ReactNode } from 'react';
+import React, { PropsWithChildren } from 'react';
 import { useTableComponentsContext } from '../TableComponentsContext';
 
-interface HeaderCellOwnProps {
-  header: ReactNode;
-}
+interface HeaderCellOwnProps {}
 
 export type HeaderCellProps = PropsWithChildren<HeaderCellOwnProps>;
 
 export function HeaderCell(props: HeaderCellProps) {
+  const { children } = props;
+
   const Components = useTableComponentsContext();
 
-  return <Components.Th>{props.header}</Components.Th>;
+  return <Components.Th>{children}</Components.Th>;
 }
