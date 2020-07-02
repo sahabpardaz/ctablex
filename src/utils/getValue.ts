d44ab67e42373f67927f15f288298bbc0eea52c9
Shahab <shahab.saeeda@gmail.com>
2020-07-03 01:09:00 +0430

    refactor(getvalue): rename value arg to data

--- src/utils/getValue.ts
+++ src/utils/getValue.ts
@@ -1,21 +1,21 @@
 import { Accessor } from './accessor';
 import { path } from './path';
 
-export function getValue<D, C>(value: D, accessor: null): null;
-export function getValue<D, C>(value: D, accessor: Accessor<D, C>): C;
+export function getValue<D, C>(data: D, accessor: null): null;
+export function getValue<D, C>(data: D, accessor: Accessor<D, C>): C;
 export function getValue<D, C>(
-  value: D,
+  data: D,
   accessor: Accessor<D, C> | null,
 ): C | null;
 export function getValue<D, C>(
-  value: D,
+  data: D,
   accessor: Accessor<D, C> | null,
 ): C | null {
   if (accessor === null) {
     return null;
   }
   if (typeof accessor === 'string') {
-    return path(accessor.split('.'), value) as C;
+    return path(accessor.split('.'), data) as C;
   }
-  return accessor(value);
+  return accessor(data);
 }
