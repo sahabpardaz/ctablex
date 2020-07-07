292061b493aea452bb7e1a1eedc55f5ccdbddbf7
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-07-07 15:03:12 +0430

    fix: fix faker seed

--- examples/basic/src/data.ts
+++ examples/basic/src/data.ts
@@ -1,7 +1,7 @@
 import faker from 'faker';
 import { Item } from './types';
 
-export function generateData(n: number, seed: number = 0): Item[] {
+export function generateData(n: number, seed: number = 1): Item[] {
   faker.seed(seed);
   return Array.from(Array(n)).map((_, index) => {
     return {
